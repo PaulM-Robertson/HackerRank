c703b43af8fcfa0b42ca48b32a1885661aa5f318
PaulM-Robertson <paulmartin.robertson@gmail.com>
2019-11-12 11:51:46 -0700

    very big summation using js

---
+++ Beginner Challenges/VeryBigSum.js
@@ -0,0 +1,8 @@
+// Complete the aVeryBigSum function below.
+function aVeryBigSum(ar) {
+    let sumTotal = 0;
+    for( let i = 0; i < ar.length; i++ ){
+        sumTotal += ar[i];
+    }
+    return sumTotal;
+}
