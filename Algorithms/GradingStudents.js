172cc51ca980bd45b3a8fed2825ca9d510b8400b
PaulM-Robertson <paulmartin.robertson@gmail.com>
2019-11-30 01:07:21 -0700

    A few sugary changes

--- Algorithms/GradingStudents.js
+++ Algorithms/GradingStudents.js
@@ -31,7 +31,7 @@ function readLine() {
 
 function gradingStudents(grades) {
     return grades.map(function(grade) {
-        return ( grade >= 38 && grade % 5 >= 3) ? grade + 5 - (grade % 5) : grade;
+        return ( grade >= 38 && grade % 5 >= 3 ) ? grade + 5 - (grade % 5) : grade;
     });
 }
 
