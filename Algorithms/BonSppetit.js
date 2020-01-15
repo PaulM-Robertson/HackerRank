aa8f8d73b696dec6ba0daf19ef3c098d216bf017
PaulM-Robertson <paulmartin.robertson@gmail.com>
2020-01-15 10:50:10 -0700

    Bon appetit

---
+++ Algorithms/BonSppetit.js
@@ -0,0 +1,20 @@
+
+function bonAppetit(bill, k, b) {
+    //add bill array together
+    //subtract item k
+    //check if bill-k === b
+    //if yes, print "bon appetit"
+    //if no, print difference 
+    for (let i = 0; i < bill.length; i++) {
+        if ()
+    }
+
+}
+
+
+(()=>{
+    let bill = [12,4,8];
+    let k = 1;
+    let b = 12;
+    console.log('bonappetit(bill, k, b)' , bonAppetit(bill, k, b))
+})()
