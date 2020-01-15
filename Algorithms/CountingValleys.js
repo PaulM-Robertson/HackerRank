9edf16bc2de48437644ca9c07df9c3bcaeb79281
PaulM-Robertson <paulmartin.robertson@gmail.com>
2020-01-15 11:13:25 -0700

    counting valleys

---
+++ Algorithms/CountingValleys.js
@@ -0,0 +1,10 @@
+function countingValleys(n, s) {
+
+
+}
+
+(()=>{
+    let n = 100;
+    let s = 'DDUUUUUUUUUUUUUDDDUUDDUUDDDDDDDDD';
+    console.log('countingvalleys(n, s)' , countingValleys(n, s))
+})()
