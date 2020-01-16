9b13c3eb0eafbd7e27e7c13db26173df2b4caa42
PaulM-Robertson <paulmartin.robertson@gmail.com>
2020-01-16 10:38:37 -0700

    breaking records with colin logic

--- Algorithms/BreakingRecords.js
+++ Algorithms/BreakingRecords.js
@@ -1,9 +1,18 @@
 function breakingRecords(scores) {
-
-
+    let maxIncrease = 0,
+        minDecrease = 0,
+        max = scores[0],
+        min = scores[0];
+    for (let i = 0; i < scores.length; i++) {
+        if (scores[i] > max)  maxIncrease += 1, max = scores[i];
+        if (scores[i] < min)  minDecrease += 1, min = scores[i]; 
+    }
+    return [maxIncrease, minDecrease];
 }
 
 (()=>{
-    
+    scores = [12,13,9,5,23,3,34];
     console.log('breakingRecords(scores)' , breakingRecords(scores))
 })()
+
+[12,23,13]
