eb1798c6882c3d31abc07d1030d745ef783cbbd5
PaulM-Robertson <paulmartin.robertson@gmail.com>
2020-01-16 08:24:58 -0700

    Counting Valleys challenge

--- Algorithms/CountingValleys.js
+++ Algorithms/CountingValleys.js
@@ -1,10 +1,28 @@
+
+
 function countingValleys(n, s) {
-
-
+    let arr = s.split('');
+    let down = 0,
+        up = 0,
+        valleys = 0;
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === "D") {
+            down += 1;
+        } else {
+            up += 1;
+        }
+    }
+    console.log(elevation);
 }
 
 (()=>{
-    let n = 100;
-    let s = 'DDUUUUUUUUUUUUUDDDUUDDUUDDDDDDDDD';
+    let n = 24;
+    let s = 'DDUUUUUUDDDUUDDUUUDDDDUU';
     console.log('countingvalleys(n, s)' , countingValleys(n, s))
 })()
+//        /\
+//       /  \  /\ 
+//      /    \/  \
+//_    /          \    _
+// \  /            \  / 
+//  \/              \/   
