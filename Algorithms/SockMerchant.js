75c2d86ae3603909f04906e80aa450ed30c86106
PaulM-Robertson <paulmartin.robertson@gmail.com>
2020-01-16 13:07:47 -0700

    sock merchant final draft

--- Algorithms/SockMerchant.js
+++ Algorithms/SockMerchant.js
@@ -1,8 +1,15 @@
 function sockMerchant(n, ar) {
-    let sorted = ar.sort((a,b) => {return a-b})
-    
+    let pairs = 0;
+    let sorted = ar.sort((a,b) => {return a-b});
+    for (let i = 0; i < (n - 1); i++) {
+        if (sorted[i] === sorted[i + 1])  pairs += 1, sorted[i + 1] = 'paired';
+    }
+    return pairs;
 }
 
 (()=>{
+    let n = 9;
+    let ar = [10, 10, 10, 10, 20, 20, 20, 30, 50];
+
     console.log('sockMerchant(n, ar)', sockMerchant(n, ar))
 })()
