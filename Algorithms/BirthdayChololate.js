ede4ae5d15c57a3b37d336bd01551a00d54e3af4
PaulM-Robertson <paulmartin.robertson@gmail.com>
2020-01-23 10:00:39 -0700

    finished birthday chocolate challenge using nested for loops

--- Algorithms/BirthdayChololate.js
+++ Algorithms/BirthdayChololate.js
@@ -1,13 +1,21 @@
 // Complete the birthday function below.
 function birthday(s, d, m) {
-   
-
-
+   let total = 0;
+   for (i = 0; i <= (s.length)-m; i++) {
+      let sum = 0;
+      for (j = 0; j < m; j++) {
+         sum += s[i+j]; 
+      }
+      if (sum === d) total++;
+   }
+   return total;
 }
+
+
 
 (()=>{
    let s = [1,4,2,3];
    let d = 5;
    let m = 2;
-   console.log( "birthday(s,d,m)", birthday(s, d. m) );
+   console.log('birthday(s, d, m)' , birthday(s, d, m))
 })()
