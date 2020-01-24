67845341d2d42899c6b47f75fd3ed35f3167634b
PaulM-Robertson <paulmartin.robertson@gmail.com>
2020-01-24 13:09:16 -0700

    the code works but i get a runtime error when i submit it. too many for loops i guess

--- Algorithms/ElectronicStore.js
+++ Algorithms/ElectronicStore.js
@@ -1,13 +1,21 @@
 function getMoneySpent(keyboards, drives, b) {
-    /*
-     * Write your code here.
-     */
-
+    let arr = [];
+    let maxCost = 0;
+    for (let i = 0; i < keyboards.length; i++) {
+        for (let j = 0; j < drives.length; j++) {
+            arr.push(keyboards[i] + drives[j]);     
+        }  
+    }  
+    for (let i = 0; i < arr.length; i++ ) {
+        if (arr[i] > b) arr[i] = -1;
+    }
+    maxCost += Math.max(...arr);
+    return maxCost;
 }
 
 (()=>{
-    let keyboards = [];
-    let drives = [];
-    let b = 100
+    let keyboards = [3,1];
+    let drives = [5,2,8];
+    let b = 10
     console.log('getMoneySpent(keyboards, drives, b)', getMoneySpent(keyboards, drives, b))
 })()
