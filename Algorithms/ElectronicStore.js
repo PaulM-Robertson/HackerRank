83db438c97a34167b2f550ce5a2ed4f78c6eb8cf
PaulM-Robertson <paulmartin.robertson@gmail.com>
2020-01-24 10:51:41 -0700

    Set up coding environment for Hacker Rank challenge

---
+++ Algorithms/ElectronicStore.js
@@ -0,0 +1,13 @@
+function getMoneySpent(keyboards, drives, b) {
+    /*
+     * Write your code here.
+     */
+
+}
+
+(()=>{
+    let keyboards = [];
+    let drives = [];
+    let b = 100
+    console.log('getMoneySpent(keyboards, drives, b)', getMoneySpent(keyboards, drives, b))
+})()
