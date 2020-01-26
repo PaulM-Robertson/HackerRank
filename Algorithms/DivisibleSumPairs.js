69c2767c50a9f7a1deedb85fe54de4c38291070f
PaulM-Robertson <paulmartin.robertson@gmail.com>
2020-01-25 21:39:30 -0700

    still working on this challenge. looking at nested for loops. day 2 of 100 days of coding challenge.

--- Algorithms/DivisibleSumPairs.js
+++ Algorithms/DivisibleSumPairs.js
@@ -1,12 +1,19 @@
 function divisibleSumPairs(n, k, ar) {
-    for (let i = 0; i < n; i++) {
-        
+    let cocks = 0;
+    for (let i = 0; i < k.length; i++) {
+        for (let j = 1; j < k.length; j++) {
+            if (ar[i] + ar[j] % k === 0) cocks += 1;
+        }
+        return cocks;
     }
+    console.log( cocks);
     
 }
 
 (()=>{
     let ar = [2,5,7,4,12];
+    let n = ar.length;
     let k = 9;
+    // output = 2
     console.log('divisibleSumPairs(n, k, ar)', divisibleSumPairs(n, k, ar))
 })()
