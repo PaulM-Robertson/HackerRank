890a4416f8d44ac32cfb4155f1a8cc80ae96d0d1
PaulM-Robertson <paulmartin.robertson@gmail.com>
2020-01-25 21:25:31 -0700

    set up coding environment for Divisible Sum Pairs Hacker rank challenge.

---
+++ Algorithms/DivisibleSumPairs.js
@@ -0,0 +1,12 @@
+function divisibleSumPairs(n, k, ar) {
+    for (let i = 0; i < n; i++) {
+        
+    }
+    
+}
+
+(()=>{
+    let ar = [2,5,7,4,12];
+    let k = 9;
+    console.log('divisibleSumPairs(n, k, ar)', divisibleSumPairs(n, k, ar))
+})()
