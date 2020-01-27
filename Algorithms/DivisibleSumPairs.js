6642a7bdae7cc3f91cdd92eb31fded85581007d9
PaulM-Robertson <paulmartin.robertson@gmail.com>
2020-01-26 20:40:29 -0700

    asdf

--- Algorithms/DivisibleSumPairs.js
+++ Algorithms/DivisibleSumPairs.js
@@ -1,19 +1,19 @@
 function divisibleSumPairs(n, k, ar) {
     let cocks = 0;
     for (let i = 0; i < n; i++) {
-        for (let j = 1; j < n+1; j++) {
-            if ((ar[i] + ar[j]) % k === 0) cocks += 1;
+        for (let j = 1; j <= n; j++) {
+            let a = 0;
+            if (i > j && i != j) a = ar[i] + ar[j];
+            if (a % k === 0) cocks++;
         }
-        return cocks;
     }
     console.log( cocks);
-    
 }
 
 (()=>{
-    let ar = [2,5,7,4,12];
+    let ar = [2,4,6];
     let n = ar.length;
-    let k = 9;
-    // output = 2
+    let k = 2;
+    // output = 3
     console.log('divisibleSumPairs(n, k, ar)', divisibleSumPairs(n, k, ar))
 })()
